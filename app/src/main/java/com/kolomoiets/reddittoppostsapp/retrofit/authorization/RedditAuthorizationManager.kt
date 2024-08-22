a577f4814bcdf81c53908751941f4c7de204ab6e
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 14:15:45 +0300

    Created files 'RedditManager', 'RedditPostManager'. Renamed file 'RedditAuthorizationImpl'

---
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/retrofit/authorization/RedditAuthorizationManager.kt
@@ -0,0 +1,5 @@
+package com.kolomoiets.reddittoppostsapp.retrofit.authorization
+
+class RedditAuthorizationManager {
+
+}
---
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/retrofit/posts/RedditPostManager.kt
@@ -0,0 +1,4 @@
+package com.kolomoiets.reddittoppostsapp.retrofit.posts
+
+class RedditPostManager {
+}
