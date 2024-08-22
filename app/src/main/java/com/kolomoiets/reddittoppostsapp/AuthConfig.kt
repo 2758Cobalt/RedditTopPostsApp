a185b6e9d0e949b99aee639891b5a02cb619efcd
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 16:00:54 +0300

    Created Authorization config object

---
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/AuthConfig.kt
@@ -0,0 +1,8 @@
+package com.kolomoiets.reddittoppostsapp
+
+object AuthConfig {
+    const val baseURL = "https://www.reddit.com"
+    const val grantType = "client_credentials"
+    const val username = "jffHdkgd3_vbD_s2ftdguw"
+    const val password = "OYxditQQ5GPGx1EMrognHHxgsIRXoQ"
+}
