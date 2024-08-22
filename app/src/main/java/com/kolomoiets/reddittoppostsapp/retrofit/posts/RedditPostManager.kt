95b9f2e51579ce97d43fbe4110f91a2e0e8c3ef0
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 15:19:36 +0300

    Configured 'RedditPostManager'

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/retrofit/posts/RedditPostManager.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/retrofit/posts/RedditPostManager.kt
@@ -1,4 +1,69 @@
 package com.kolomoiets.reddittoppostsapp.retrofit.posts
 
-class RedditPostManager {
+import android.util.Log
+import com.cobaltumapps.reddittoppostsappblank.retrofit.posts.data.PostResponse
+import com.kolomoiets.reddittoppostsapp.retrofit.authorization.data.AuthResponseData
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.data.RedditPostParams
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.data.toMap
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.interfaces.RedditPostApi
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.interfaces.RedditPostListener
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RedditPostManager: RedditPostListener {
+    private var baseUrl : String = "https://oauth.reddit.com/"
+
+    private var redditPostListener: RedditPostListener? = null
+    private var authResponseData: AuthResponseData? = null
+
+    private var redditPostsApi : RedditPostApi
+
+    init {
+        val httpInterceptor = HttpLoggingInterceptor()
+        httpInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val clientHttp = OkHttpClient.Builder()
+            .addInterceptor(httpInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clientHttp)
+            .build()
+
+        redditPostsApi = retrofit.create(RedditPostApi::class.java)
+    }
+
+    fun setNewListener(listener: RedditPostListener) {
+        this.redditPostListener = listener
+    }
+
+    fun setAuthData(newAuthResponseData: AuthResponseData) {
+        this.authResponseData = newAuthResponseData
+    }
+
+    override suspend fun getTopPosts(redditPostParams: RedditPostParams): PostResponse? {
+        val call = if (authResponseData != null) {
+            authResponseData?.accessToken?.let {
+                redditPostsApi.getTopPosts(
+                    authorizationHeader = it,
+                    queryMap = redditPostParams.toMap()
+                )
+            }
+        }
+        else {
+            Log.e(LOG_TAG, "Auth response is null")
+            null
+        }
+
+        return call?.execute()?.body()
+    }
+
+
+    companion object {
+        const val LOG_TAG = "RedditPostManagerDebugTag"
+    }
 }
+
