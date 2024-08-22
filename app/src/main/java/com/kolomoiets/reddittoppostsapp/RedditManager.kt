7e3f56fffa52af99fce3f5a579ba848aea8dbb37
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 15:20:26 +0300

    Configured 'RedditManager'

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/RedditManager.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/RedditManager.kt
@@ -1,4 +1,23 @@
 package com.kolomoiets.reddittoppostsapp
 
-class RedditManager {
+import com.cobaltumapps.reddittoppostsappblank.retrofit.posts.data.PostResponse
+import com.kolomoiets.reddittoppostsapp.retrofit.authorization.RedditAuthorizationManager
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.RedditPostManager
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.data.RedditPostParams
+import com.kolomoiets.reddittoppostsapp.retrofit.posts.interfaces.RedditPostListener
+
+class RedditManager: RedditPostListener {
+    private val redditAuthorizationManager = RedditAuthorizationManager()
+    private val redditPostManager = RedditPostManager()
+
+    init {
+        redditPostManager.setNewListener(this)
+    }
+
+    override suspend fun getTopPosts(redditPostParams: RedditPostParams): PostResponse? {
+        val token = redditAuthorizationManager.getAuthResponse() ?: redditAuthorizationManager.authorization()
+        token?.let { redditPostManager.setAuthData(it) }
+
+        return redditPostManager.getTopPosts(redditPostParams)
+    }
 }
