8082e964e7527e2c1c16ccf33c3ac410acbe10e8
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 15:20:09 +0300

    Configured 'RedditAuthorizationManager'

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/retrofit/authorization/RedditAuthorizationManager.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/retrofit/authorization/RedditAuthorizationManager.kt
@@ -1,5 +1,77 @@
 package com.kolomoiets.reddittoppostsapp.retrofit.authorization
 
-class RedditAuthorizationManager {
+import android.util.Base64
+import android.util.Log
+import com.kolomoiets.reddittoppostsapp.fileStream.AuthConfig
+import com.kolomoiets.reddittoppostsapp.retrofit.authorization.data.AuthRequestData
+import com.kolomoiets.reddittoppostsapp.retrofit.authorization.data.AuthResponseData
+import com.kolomoiets.reddittoppostsapp.retrofit.authorization.interfaces.RedditAuthorizationApi
+import com.kolomoiets.reddittoppostsapp.retrofit.authorization.interfaces.RedditAuthorizationListener
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.FileInputStream
+import java.util.Properties
 
+class RedditAuthorizationManager: RedditAuthorizationListener {
+    protected var baseUrl : String = "https://www.reddit.com/"
+
+    private var authConfig: AuthRequestData = AuthConfig().getAuthConfig()
+    private var authResponseData: AuthResponseData? = null
+    private var redditAuthorizationAPI : RedditAuthorizationApi
+
+    init {
+
+        val httpInterceptor = HttpLoggingInterceptor()
+        httpInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val clientHttp = OkHttpClient.Builder()
+            .addInterceptor(httpInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clientHttp)
+            .build()
+
+        redditAuthorizationAPI = retrofit.create(RedditAuthorizationApi::class.java)
+
+    }
+
+    override suspend fun authorization(): AuthResponseData? {
+        val credentials = with(authConfig) {
+            "$username:$password"
+        }
+        val authHeader = "Basic " + Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
+
+
+        val call = redditAuthorizationAPI.authorization(
+            authHeader,
+            hashMapOf(Pair("grant_type", authConfig.grantType)))
+
+        authResponseData = call.execute().body()
+
+        authResponseData?.apply {
+            Log.i(
+                LOG_TAG,
+                "Auth body\n" +
+                        "access_token: ${accessToken}\n" +
+                        "token_type: $tokenType\n" +
+                        "expires_in: $expiresIn\n" +
+                        "scope: $scope")
+        }
+
+        return authResponseData
+    }
+
+    fun getAuthResponse(): AuthResponseData? {
+        return this.authResponseData
+    }
+
+    companion object {
+        const val LOG_TAG = "RedditAuthorizationDebugTag"
+    }
 }
+
