8e47e7b19b94cd98a85e065775a6467e6dafef38
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 18:42:13 +0300

    Added function 'Save to gallery' in 'MainActivity' class

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/MainActivity.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.kolomoiets.reddittoppostsapp
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.cobaltumapps.reddittoppostsappblank.retrofit.posts.data.PostResponse
 import com.kolomoiets.reddittoppostsapp.adapters.redditPosts.RedditPostListAdapter
 import com.kolomoiets.reddittoppostsapp.adapters.redditPosts.ViewHolderPostListener
 import com.kolomoiets.reddittoppostsapp.data.RedditPostData
 import com.kolomoiets.reddittoppostsapp.databinding.ActivityMainBinding
+import com.kolomoiets.reddittoppostsapp.imageSaver.PicassoImageSaver
 import com.kolomoiets.reddittoppostsapp.retrofit.posts.data.RedditPostParams
 import com.kolomoiets.reddittoppostsapp.retrofit.posts.data.subdata.RedditApiTime
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Picasso.LoadedFrom
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
 
 class MainActivity : AppCompatActivity(), ViewHolderPostListener {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var postAdapter: RedditPostListAdapter
     private lateinit var postManager: RedditManager
+
+    private var picassoImageSaver = PicassoImageSaver(this)
 
     private val LIST_STATE_KEY = "reddit_post_list_state"
 
@@ -48,7 +63,6 @@ class MainActivity : AppCompatActivity(), ViewHolderPostListener {
         binding.recyclerViewMain.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
-
                 if (!recyclerView.canScrollVertically(1)) {
                     Log.i(LOG_TAG, postAdapter.getLastItemId())
                     loadData(
@@ -69,6 +83,7 @@ class MainActivity : AppCompatActivity(), ViewHolderPostListener {
             val savedRedditPosts =  savedInstanceState.getParcelableArrayList<RedditPostData>(LIST_STATE_KEY)?.toMutableList() ?: mutableListOf<RedditPostData>()
             postAdapter.setNewItemsList(savedRedditPosts)
         }
+
     }
 
     // Збереження списку елементів у RecyclerView
@@ -108,6 +123,9 @@ class MainActivity : AppCompatActivity(), ViewHolderPostListener {
         startActivity(intent)
     }
 
+    override fun actionSaveToGallery(thumbnailUrl: String) {
+        picassoImageSaver.saveImageToGallery(thumbnailUrl)
+    }
 
     companion object {
         const val LOG_TAG = "MainActivityTag"
