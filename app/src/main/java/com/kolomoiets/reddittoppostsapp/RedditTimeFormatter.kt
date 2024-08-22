e71a9deb0c8b60a29e89a06992c9083a9dd4ffde
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 19:02:01 +0300

    Fixed creation time display

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/RedditTimeFormatter.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/RedditTimeFormatter.kt
@@ -4,19 +4,22 @@ import android.content.Context
 import java.util.concurrent.TimeUnit
 
 class RedditTimeFormatter(private val context: Context) {
-    private enum class TimeTypes { hour, day }
+    private enum class TimeTypes { minute, hour, day }
 
     fun formatTime(unixTime: Number): String {
         val currentTime = System.currentTimeMillis() / 1000
         val timeDifference = currentTime - unixTime.toLong()
 
-        val hours = TimeUnit.SECONDS.toHours(timeDifference)
         val days = TimeUnit.SECONDS.toDays(timeDifference)
+        val hours = TimeUnit.SECONDS.toHours(timeDifference)
+        val minutes = TimeUnit.SECONDS.toMinutes(timeDifference)
 
         return when {
-            hours > 0 -> context.getString(R.string.post_dateTime).format(hours, TimeTypes.hour.name + (if (hours > 1) TimeTypes.hour.name + "s" else ""))
-            days > 0 -> context.getString(R.string.post_dateTime).format(days, TimeTypes.day.name + (if (days > 1) TimeTypes.day.name + "s" else ""))
-            else -> {"just now"}
+            days > 0 -> context.getString(R.string.post_dateTime).format(days, if (days > 1) TimeTypes.day.name + "s" else TimeTypes.day.name)
+            hours > 0 -> context.getString(R.string.post_dateTime).format(hours, if (hours > 1) TimeTypes.hour.name + "s" else TimeTypes.hour.name)
+            minutes > 0 -> context.getString(R.string.post_dateTime).format(minutes, if (minutes > 1) TimeTypes.minute.name + "s" else TimeTypes.minute.name)
+//
+            else -> { context.getString(R.string.post_justNow) }
         }
     }
 }
