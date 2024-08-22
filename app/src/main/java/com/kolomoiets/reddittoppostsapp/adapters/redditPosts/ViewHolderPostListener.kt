66e66d6f69d2c4402cd997316b20765495798c4c
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 18:36:48 +0300

    Added method for saving thumbnail to gallery

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/adapters/redditPosts/ViewHolderPostListener.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/adapters/redditPosts/ViewHolderPostListener.kt
@@ -1,5 +1,8 @@
 package com.kolomoiets.reddittoppostsapp.adapters.redditPosts
 
+import android.graphics.Bitmap
+
 interface ViewHolderPostListener {
     fun onThumbnailClick(thumbnailUrl: String)
+    fun actionSaveToGallery(thumbnailUrl: String)
 }
