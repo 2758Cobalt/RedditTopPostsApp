6a73319533afd0cec3fdd3e42d9e881b4ecf9efd
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 17:36:25 +0300

    prepared 'RedditPostData' date class for saving

--- app/src/main/java/com/kolomoiets/reddittoppostsapp/data/RedditPostData.kt
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/data/RedditPostData.kt
@@ -1,12 +1,47 @@
 package com.kolomoiets.reddittoppostsapp.data
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /* stores information about the post */
 
-data class RedditPostData (
+data class RedditPostData(
     val id: String,
     val authorName: String,
     val title: String,
     val createdTime: Double,
     val thumbnailUrl: String,
     val commentsCount: Int
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readDouble(),
+        parcel.readString() ?: "",
+        parcel.readInt(),
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(authorName)
+        parcel.writeString(title)
+        parcel.writeDouble(createdTime)
+        parcel.writeString(thumbnailUrl)
+        parcel.writeInt(commentsCount)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RedditPostData> {
+        override fun createFromParcel(parcel: Parcel): RedditPostData {
+            return RedditPostData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RedditPostData?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
