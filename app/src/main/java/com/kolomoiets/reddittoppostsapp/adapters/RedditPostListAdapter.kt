151a54b84e4926ab171d7278b515ea7d17c81dc9
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-08-22 13:21:30 +0300

    Setup 'RedditPostListAdapter'

---
+++ app/src/main/java/com/kolomoiets/reddittoppostsapp/adapters/RedditPostListAdapter.kt
@@ -0,0 +1,78 @@
+package com.kolomoiets.reddittoppostsapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.kolomoiets.reddittoppostsapp.data.RedditPostData
+import com.kolomoiets.reddittoppostsapp.databinding.ItemRedditPostBinding
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
+
+class RedditPostListAdapter(private val context: Context): ListAdapter<RedditPostData, RedditPostListAdapter.PostViewHolder>(Comparator()) {
+    private var postListData = mutableListOf<RedditPostData>()
+
+    private var picassoInstance = Picasso.Builder(context).build()
+    private var redditPostListener: RedditPostListener? = null
+
+    class Comparator: DiffUtil.ItemCallback<RedditPostData>() {
+        override fun areItemsTheSame(oldItem: RedditPostData, newItem: RedditPostData): Boolean {
+            return oldItem.authorName == newItem.authorName
+        }
+
+        override fun areContentsTheSame(oldItem: RedditPostData, newItem: RedditPostData): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    inner class PostViewHolder(private val itemBinding: ItemRedditPostBinding): RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(redditPostData: RedditPostData) {
+            itemBinding.apply {
+                with(redditPostData) {
+                    postId.text = id
+                    postAuthorName.text = authorName
+                    postTitle.text = title
+                    postButtonComments.text = commentsCount.toString()
+
+                    picassoInstance
+                        .load(thumbnailUrl)
+                        .into(postThumbnail, object : Callback {
+                            override fun onSuccess() { postThumbnailCard.isVisible = true }
+                            override fun onError(e: Exception?) { }
+                        })
+
+
+                    postThumbnailCard.setOnClickListener { redditPostListener?.onThumbnailClick(thumbnailUrl) }
+                }
+            }
+        }
+    }
+
+    // Setup RedditPostListener
+    fun setNewListener(listener: RedditPostListener) {
+        this.redditPostListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = ItemRedditPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding.postThumbnailCard.isVisible = false
+        return PostViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val postItem = getItem(position)
+        holder.bind(postItem)
+    }
+
+    companion object {
+        const val LOG_TAG = "RecyclerViewDebugTag"
+    }
+}
+
+interface RedditPostListener {
+    fun onThumbnailClick(thumbnailUrl: String)
+}
